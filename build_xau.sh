e0ea8493529b4c70a09ba4879baf753053a380f0
Antonio Dell'Elce <antouk@dellelce.com>
2019-05-26 16:57:09 +0100

    scripts: xau xproto

---
+++ build_xau.sh
@@ -0,0 +1,12 @@
+
+## ENV ##
+
+id="libXau-1.0.9"
+dir="/tmp/opengl/$id"
+prefix="/tmp/opengl/i_0"
+
+## MAIN ##
+
+cd "$dir" || exit $?
+export PKG_CONFIG_PATH=${prefix}/lib/pkgconfig
+./configure --prefix=${prefix}  && make && make install
